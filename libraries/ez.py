201e1d867d5667676bf1183b0c08ad708cb604b6
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2023-05-28 08:05:59 +0800

    centralized management for info used in program
    for easier synchronization handling and prevent sensitive information exposed to public
    
    so now every time the program will locate the folder with the same name as the script inside the centralized folder "info for program", and then open the info files inside

--- libraries/ez.py
+++ libraries/ez.py
@@ -4,9 +4,9 @@ import time
 import sys
 import keyboard
 import os
-import random
 import tqdm
 import shutil
+import subprocess
 
 # 按住大写键停止程序
 def check_force_stop():
@@ -44,3 +44,42 @@ def copy_file(source,destination):
 def pause(message=None):
 	print (message)
 	os.system("pause")
+
+def get_repository_name():
+	try:
+		# get the repository url from local git config file
+		output = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url'])
+		# convert bytes to string and split by '/'
+		url_parts = output.decode().strip().split('/')
+		repository_name = url_parts[-1].replace('.git', '')
+		return repository_name
+	
+	# if any exception of these types is raised during execution, the corresponding except block will handle it
+		# subprocess.CalledProcessError
+			# execute a command that does not exist
+			# i.e. git is not yet installed
+		# IndexError
+			# try to access an index that is out of range
+			# unlikely to happen, but still may happen in "url_parts[-1]"
+	except (subprocess.CalledProcessError, IndexError):
+		return None
+
+# get the corresponding info folder for each script
+# assume
+	# the name of the info folder is "info for program"
+	# the name of the corresponding folder inside info folder is the same as the script file name
+def get_info_path(program_file_path):
+	git_repo_name = get_repository_name()
+	info_folder = "info for program"
+	# get the root path of the git repository
+	root,script_folder = program_file_path.split(f"\\{git_repo_name}")
+	script_folder = script_folder[1:] # remove the '\' at the beginning
+	if (script_folder==""): # when the script is in the root folder
+		script_folder = root
+
+	# pause("root: "+root)
+	# pause("script: "+script_folder)
+
+	info_path = f"{root}\\{git_repo_name}\\{info_folder}\\{script_folder}"
+
+	return info_path
---
+++ test.py
@@ -0,0 +1,4 @@
+from ez import get_info_path,get_repository_name
+import os
+
+print (get_info_path(os.path.abspath(__file__)))
