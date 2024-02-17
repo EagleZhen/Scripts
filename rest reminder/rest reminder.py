2d2f451a486c0c70f4ae57469a05d3fffc08dd41
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-02-17 15:31:11 +0800

    Rewrite rest reminder to incorporate with NetEase Cloud Music Player
    It would automatically play music and lock the screen to prevent any further work.

--- rest reminder/rest reminder.py
+++ rest reminder/rest reminder.py
@@ -1,56 +1,49 @@
-import time
-import os
-import random
-import keyboard
-import ez
+from time import sleep
+from keyboard import send
+import ctypes
 import subprocess
+from datetime import datetime, timedelta
+from ez import notify, print_divider
 
-def random_play_song():
-	path="E:/Data/Music/网易云音乐/"
-	file_list=os.listdir(path)
 
-	song=""
-	while (song.find(".mp3")==-1):
-		song=random.choice(file_list)
+def launch_music_player() -> None:
+    # r"" is used to convert the string to raw string
+    music_player_path = r"C:\Program Files (x86)\NetEase\CloudMusic\cloudmusic.exe"
+    subprocess.Popen(music_player_path)
 
-	# os.startfile(path+song)
-	subprocess.Popen(["C:/Apps/QQPlayer/QQPlayer.exe",path+song])
-	print(song)
 
-	time.sleep(300)
-	os.system("taskkill /f /im QQPlayer.exe")
+def play_next_song() -> None:
+    # Press "Ctrl+Alt+Right Arrow" to play the next song in my music player
+    send('ctrl+alt+right')
 
-	# keyboard.send("alt+tab")
-	# choice=int ( input("0:ok | 1:next song | 2:I don't wanna rest yet\n") )
 
-	
-	# if (choice==0):
-	# 	keyboard.send("alt+tab")
-	# 	time.sleep(0.5)
-	# 	keyboard.send("windows+down")
-	# 	keyboard.send("windows+down")
+def lock_screen():
+    # Locks the screen on Windows
+    ctypes.windll.user32.LockWorkStation()
 
-	# elif (choice==1):
-	# 	os.system("taskkill /f /im QQPlayer.exe")
-	# 	random_play_song()
 
-	# elif (choice==2):
-	# 	os.system("taskkill /f /im QQPlayer.exe")
+def reminder(interval_minutes: float) -> None:
+    while True:
+        print_divider()
 
-def timer():
-	total_minutes=int( input("minutes = ") )
+        next_rest_time = datetime.now() + timedelta(minutes=interval_minutes)
+        print(f"Next break at {next_rest_time.strftime('%m-%d %H:%M:%S')}.")
 
-	# keyboard.send("windows+d")
-	while(True):
-		for minutes in range(total_minutes):
-			print (total_minutes-minutes," minutes left! Please add oil!",sep="")
-			time.sleep(60)
+        sleep(interval_minutes * 60)  # Convert minutes to seconds
+        notify(title="Rest Reminder", message="Time for a break!")
 
-			# for seconds in range(60):
-			# 	print (total_minutes-minutes-1,":",59-seconds,sep="")
-			# 	time.sleep(1)
+        launch_music_player()
+        sleep(10)  # Give me some time to respond before playing songs suddenly 🤣
+        play_next_song()
+        print("Playing next song")
 
-		random_play_song()
+        sleep(10)
+        print("Locking screen")
+        lock_screen()
 
-timer()
-# random_play_song()
+        sleep(5*60)  # 5 minutes break
+
+
+if __name__ == "__main__":
+    interval_minutes = float(input("Enter the reminder interval in minutes: "))
+    reminder(interval_minutes)
