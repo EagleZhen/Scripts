05731131aa674f2ce3f18038a5122ec3abee217a
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-03-24 20:58:00 +0800

    moved the log file to the folder "info for program" & modified log content

--- rest reminder/rest reminder.py
+++ rest reminder/rest reminder.py
@@ -3,7 +3,8 @@ from keyboard import send
 import ctypes
 import subprocess
 from datetime import datetime, timedelta
-from ez import notify, print_divider
+from ez import notify, print_divider, get_info_path
+from os.path import join
 
 def print_message(message, write_to_log=True, need_notification=False, title=""):
     # Show the timestamps for the corresponding status code
@@ -13,7 +14,7 @@ def print_message(message, write_to_log=True, need_notification=False, title="")
     print(formatted_datetime, "|", message)
 
     if write_to_log:
-        with open("./log.txt", "a", encoding="utf-8") as log:
+        with open(log_file_path, "a", encoding="utf-8") as log:
             log.write(formatted_datetime + " | " + message + "\n")
 
     if need_notification is True:
@@ -44,7 +45,9 @@ def reminder(interval_minutes: float) -> None:
         print_message(f"Next break at {next_rest_time.strftime('%m-%d %H:%M:%S')}.")
 
         sleep(interval_minutes * 60)  # Convert minutes to seconds
-        print_message("Time to take a break!", need_notification=True, title="Rest Reminder", write_to_log=False)
+        # come back after 5 minutes
+        back_time = (datetime.now() + timedelta(minutes=5)).strftime("%H:%M")
+        print_message(f"Time to take a break! 🍵", need_notification=True, title="Rest Reminder")
 
         launch_music_player()
         sleep(15)  # Give me some time to respond before playing songs suddenly 🤣
@@ -57,7 +60,15 @@ def reminder(interval_minutes: float) -> None:
 
         sleep(5*60)  # 5 minutes break
 
+log_file_path = join(get_info_path(),"log.txt")
 
 if __name__ == "__main__":
     interval_minutes = float(input("Enter the reminder interval in minutes: "))
+    
+    # separate the logs for each run
+    with open(log_file_path, "a", encoding="utf-8") as log:
+        log.write(f"{'='*10}")
+        log.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " | " + f"Rest reminder started with interval of {interval_minutes} minutes")
+        log.write(f"{'='*10}\n")
+    
     reminder(interval_minutes)
