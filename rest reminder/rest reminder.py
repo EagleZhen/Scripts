62351b52c345fdda944ecb2e5d129d9ffd6bda99
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-03-24 11:29:46 +0800

    integrated the print_message() function from ureply project to this script

--- rest reminder/rest reminder.py
+++ rest reminder/rest reminder.py
@@ -5,6 +5,20 @@ import subprocess
 from datetime import datetime, timedelta
 from ez import notify, print_divider
 
+def print_message(message, write_to_log=True, need_notification=False, title=""):
+    # Show the timestamps for the corresponding status code
+    current_datetime = datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    # print timestamps with the message
+    print(formatted_datetime, "|", message)
+
+    if write_to_log:
+        with open("./log.txt", "a", encoding="utf-8") as log:
+            log.write(formatted_datetime + " | " + message + "\n")
+
+    if need_notification is True:
+        notify(title=title, message=message)
+
 
 def launch_music_player() -> None:
     # r"" is used to convert the string to raw string
@@ -27,18 +41,18 @@ def reminder(interval_minutes: float) -> None:
         print_divider()
 
         next_rest_time = datetime.now() + timedelta(minutes=interval_minutes)
-        print(f"Next break at {next_rest_time.strftime('%m-%d %H:%M:%S')}.")
+        print_message(f"Next break at {next_rest_time.strftime('%m-%d %H:%M:%S')}.")
 
         sleep(interval_minutes * 60)  # Convert minutes to seconds
-        notify(title="Rest Reminder", message="Time for a break!")
+        print_message("Time to take a break!", need_notification=True, title="Rest Reminder", write_to_log=False)
 
         launch_music_player()
-        sleep(10)  # Give me some time to respond before playing songs suddenly 🤣
+        sleep(15)  # Give me some time to respond before playing songs suddenly 🤣
         play_next_song()
-        print("Playing next song")
+        print_message("Playing the next song.", write_to_log=False)
 
-        sleep(10)
-        print("Locking screen")
+        sleep(15)
+        print_message("Locking the screen...", write_to_log=False)
         lock_screen()
 
         sleep(5*60)  # 5 minutes break
