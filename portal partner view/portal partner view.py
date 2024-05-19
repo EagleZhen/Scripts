002841f5b6d9e765d85155526ef0cd8e2a7d20b0
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-05-19 15:05:45 +0800

    Update portal partner view.py
    now it can automatically activate the partner view automatically

--- portal partner view/portal partner view.py
+++ portal partner view/portal partner view.py
@@ -1,9 +1,16 @@
 from time import sleep
 from keyboard import press, release, is_pressed
 
+cnt = 0
 while (True):
-    if (is_pressed("HOME")):
+    if (is_pressed("END")): # End key to stop the script
+        print("Script stopped\a")
         release("CAPSLOCK")
-        press("CAPSLOCK")
-        print("Activated\a")
-    sleep(1)
+        break
+        
+    release("CAPSLOCK")
+    press("CAPSLOCK")
+    print("Reactivated")
+    cnt = 0
+        
+    sleep(5)
