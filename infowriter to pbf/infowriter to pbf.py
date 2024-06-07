563480453481951cd31d3c6ecb975444630a1507
Eagle Zhen <31062364+EagleZhen@users.noreply.github.com>
2024-06-07 23:14:51 +0800

    update "parse_infowriter_content" for better case handling

--- infowriter to pbf/infowriter to pbf.py
+++ infowriter to pbf/infowriter to pbf.py
@@ -20,14 +20,49 @@ def parse_infowriter_content(content: str) -> list[tuple]:
     # re.MULTILINE is used to match the start and end of each line in a multi-line string
 
     # [1:-1] Discard the meaningless first and last bookmarks that is always 0:00:00
-    captured_groups = re.findall(
-        r"^(\d+:\d+:\d+).(.*?)$", content, re.MULTILINE)[1:-1]
+    # captured_groups = re.findall(
+    #     r"^(\d+:\d+:\d+).(.*?)$", content, re.MULTILINE)[1:-1]
     # print(captured_groups)
 
     # Convert time markers to milliseconds and store them in a list of tuples
-    default_text = "Record Time Marker"
-    time_markers = [(convert_to_milliseconds(
-        time), (text if text != default_text else "")) for time, text in captured_groups]
+    # time_markers = [(convert_to_milliseconds(
+    #     time), (text if text != default_text else "")) for time, text in captured_groups]
+    marker_content = None
+    time_markers = []
+    for line in content.split("\n"):
+        if line == "":
+            marker_content = ""
+        elif (
+            line.startswith("EVENT:START RECORDING")
+            or line.startswith("EVENT:STOP RECORDING")
+            or line.startswith("EVENT:RECORDING PAUSED")
+        ):
+            marker_content = None  # Skip empty lines and start/stop recording events
+        elif line.startswith("EVENT:RECORDING RESUMED"):
+            # group() gets the entire match, e.g. "for 0:00:01 seconds",
+            # while group(1) gets the first group only, e.g. "0:00:01"
+            paused_time = re.search(r"for (\d+:\d+:\d+) seconds", line).group(1)
+            marker_content = "Recording paused for " + paused_time
+        elif re.findall(r"^\d+:\d+:\d+.", line):
+            if marker_content is None:  # Skip useless event markers
+                continue
+
+            # Use re.search() instead of re.findall() as only the first match is needed, and there is only one match per line too
+            # groups() get all the groups at once as a tuple
+            time, text = re.search(r"^(\d+:\d+:\d+).(.*?)$", line).groups()
+
+            if (
+                text != "Record Time Marker" and marker_content == ""
+            ):  # Only keep the text if it is not the default text, and the current marker content is not specific event like pausing or resuming recording
+                marker_content = text
+
+            time_markers.append((convert_to_milliseconds(time), marker_content))
+
+            marker_content = None  # Reset the marker content
+        else:
+            marker_content = "Unknown Event"
+
+    print(time_markers)
 
     # Sort the list of tuples by milliseconds
     sorted_time_markers = sorted(time_markers, key=lambda x: x[0])
@@ -50,7 +85,9 @@ def convert_infowriter_to_pbf(input_file: str, output_file: str) -> None:
         content = file.read()
 
     sorted_time_markers = parse_infowriter_content(content)
-    if sorted_time_markers != []:  # Only write to .pbf if there are meaningful time markers
+    if (
+        sorted_time_markers != []
+    ):  # Only write to .pbf if there are meaningful time markers
         write_to_pbf(sorted_time_markers, output_file)
 
 
@@ -87,8 +124,7 @@ def batch_convert_infowriter_to_pbf(directory_path: str) -> None:
     print("Converted .infowriter files to .pbf if there are meaningful time markers.")
 
     # Confirm moving the .infowriter files to trash
-    confirm = input(
-        "Do you want to move the .infowriter files to trash? (y/[n]): ")
+    confirm = input("Do you want to move the .infowriter files to trash? (y/[n]): ")
     if confirm.lower() != "y":
         print("Skipping deletion...")
         return
